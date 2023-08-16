e116ad52f8a1c85fd62c46d0a8515c8fba69fc9e
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-08-16 17:12:30 -0300

    :tada:

--- scripts/Favorites.js
+++ scripts/Favorites.js
@@ -3,8 +3,28 @@
 export class Favorites {
     constructor(root){
         this.root = document.querySelector(root)
+        
+        this.load()
     }
     
+
+    load(){
+        this.entrises = [
+            {
+                login: "Lkaua22k",
+                name: "Kauã Aaujo",
+                public_repos: '32',
+                followers: '1'
+            },
+            {
+                login: "diego3g",
+                name: "Diego Fernades",
+                public_repos: '100',
+                followers: '400'
+            }
+        ]
+
+    }
 }
 
 //Class para visualizar no html
@@ -12,18 +32,56 @@ export class Favorites {
 export class FavoritesView extends Favorites {
     constructor(root){
         super(root)
-    
+        
+        this.tbody = this.root.querySelector('table tbody')
+
+
         this.update()
     }
 
     update(){
         this.RemoveAllTr()
+        
+        this.entrises.forEach((user) => {
+            const row = this.CreateRow()
+            row.querySelector('.users img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.users img').alt = `Imagem de ${user.name}`
+            row.querySelector('.users a p').textContent = `${user.login}`   
+            row.querySelector('.users a span').textContent = `${user.name}`   
+            row.querySelector('.Repositoris').textContent = user.public_repos
+            row.querySelector('.Followes').textContent = user.followers
+            this.tbody.append(row)
+        })
     }
 
-    RemoveAllTr(){
-        const tbody = this.root.querySelector('table tbody')
-        
-        tbody.querySelectorAll('tr').forEach((tr) => {
+    CreateRow(){
+        const tr = document.createElement('tr')
+
+        tr.innerHTML = `
+                    <td class="users">
+                        <img src="https://github.com/lkaua22k.png" alt="Imagem do Kauça" srcset="">
+
+                        <a href="https://github.com/lkaua22k" target="_blank">
+                            <span>Kauã Araujo</span>
+                            <p>lkaua22k</p>
+                        </a>
+                    </td>
+
+                    <td class="Repositoris">
+                        36
+                    </td>
+                    <td class="Followes">0</td>
+                    
+                    <td>
+                        <button>&times;</button>
+                    </td>
+                </tr>
+        `
+        return tr
+    }
+
+    RemoveAllTr(){        
+        this.tbody.querySelectorAll('tr').forEach((tr) => {
             tr.remove()            
         });
 
